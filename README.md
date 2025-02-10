43ecc960270e06b98ecc3b9d45e89df8733a96cf
Giovanni Branco <giovanni.branco@dtidigital.com.br>
2025-02-10 19:35:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Next-Architecture
+ Project to learn about Frontend Architecture on NextJS
---
+++ projects/web-public/pages/index.tsx
@@ -0,0 +1,10 @@
+import { sum } from '@next-architecture/utils/math/sum';
+
+export default function HomeScreen() {
+    return (
+        <div>
+            <h1>Home do Projeto</h1>
+            <p>@next-architecture/utils/math/sum: sum(2,2) = {sum(2,2)}</p>
+        </div>
+    );
+}
